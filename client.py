37940e7627e5850bd1ed94b6c528a9a7770026f9
Venkatesh2304 <96430022+Venkatesh2304@users.noreply.github.com>
2022-11-02 20:14:20 +0530

    auth and zipping

--- client.py
+++ client.py
@@ -2,45 +2,87 @@ import socket
 import sys
 import time 
 import json 
+import http.client
 import threading
 from Socket import Socket 
+
 #env variables
 server_address = ('localhost', 10000 )
-
+class DB() : 
+      _msg = []
+      def add_msg(self,to,msg) :
+          _id = len(self._msg) 
+          self._msg.append(to + " ==> " + msg)
+          return _id 
+      
 class Client(Socket) : 
       def __init__(self, _socket , addr = None ):
-          self.addr = addr  
-          self.msg_id = 0 
-          self.msg_without_reciepts = [] 
+          self.addr = addr    
+          self.db = DB()
           super().__init__(_socket)
-          self.url_maps = {"reciept" : self.reciept}
-      def send_msg(self,msg,headers ={}) :
-          headers = { "id": self.msg_id }
-          self.add_send_queue("/test",msg,headers)
-          self.msg_id += 1
-       
-      def reciept(self,body,headers) :
-          if body["msg_id"] == "" : 
+          self.url_maps = { "/login_res" : self.login_res , "/signup_res" : self.signup_res }
+          self.preauth_url_maps = self.url_maps
+    
+      def login_req(self,user,password) :
+          self.add_send_queue("/login",{"user" : user , "password" : password})
+      def login_res(self,data,headers) : 
+          if data : 
+            print("You are logged in")
+          else : 
+            print("Credientials is wrong")
+  
+      def signup_req(self,user,password) : 
+          self.add_send_queue("/signup",{"user" : user , "password" : password})
+      def signup_res(self,data,headers) : 
+          if data :
+             print("User created") 
+          else : 
+             print("Already exists")
+      
+      def send_msg(self,to,msg,headers ={}) :
+          _id  = self.db.add_msg(to,msg)
+          msg = {  "id": _id  , "to" : to , "msg" : msg }
+          self.add_send_queue("/send_msg",msg,headers)
+        
+      def read_reciept(self,data,headers) : 
+          msg = self.db._msg[data["id"]]
+          level = data["level"]
+          if level == 0 : 
+             print(f"Error Sending to Server :: {msg} ") 
+          elif  level == 1 :     
+             print(f"Message Sent to Server :: {msg} {data['time']}") 
+          elif level == 2 : 
+             print(f"Message Sent to Client :: {msg} {data['time']} ") 
+          elif level == 3 : 
+             print(f"Message Seen to Client :: {msg} {data['time']} ") 
+          else : 
+             print("Reciept Failure")
+             
+          
 
-      def Auth(self,*args) : 
-          if not self.user : 
-             (self.user , self.pwd) = args  
-             self.Send("/login", {""}) 
 
+
+             
+           
 def t(k) : 
  sock = Client( socket.socket(socket.AF_INET, socket.SOCK_STREAM) )
  sock.connect(server_address)
  sock.start()
  print(f"client {k} started")
- for i in range(10) :
-    msg = "test messsage number"*1000 +  f":: client ==> {k} ,  msg_no ==> {i}"
-    sock.add_send_queue("/test" , msg)    
-    time.sleep(0.1)
- time.sleep(100)
+ #sock.login_req(input("user : "),input("pass : "))
+ sock.login_req("ven","12"+ str(k)[0])
 
-for i in range(1) : 
-        client_thread = threading.Thread(target=t,args=(i,))
-        client_thread.start()
+ for i in range(10) :
+     msg = "test messsage number"*1000 +  f":: client ==> {k} ,  msg_no ==> {i}"
+     sock.add_send_queue("/test" , msg)    
+     time.sleep(0.1)
+ #time.sleep(100)
+
+t(1)
+
+# for i in range(100) : 
+#         client_thread = threading.Thread(target=t,args=(i,))
+#         client_thread.start()
 
 
 
--- server.py
+++ server.py
@@ -1,36 +1,79 @@
 
 
 import socketserver
+from tabnanny import check
 import threading
 import socket
-from Socket import Socket 
+import time 
+from Socket import Socket
+
+users = {"ven" :"121", "aad":"122" , "yash" : "123"}
+
+class DB() : 
+      _msg = []
+      def add_msg(self,to,msg) :
+          _id = len(self._msg) 
+          self._msg.append(to + " ==> " + msg)
+          return _id 
+
+
+
 def client_handle(conn,addr) : 
-    connection = Server(conn,addr) 
+    connection = Server(conn,addr)
     connection.start()
 
-users = [["ven","121"],["ven","122"],["ven","123"]]
+def create_user(user,password) : 
+    if user not in users : 
+       users[user] = password 
+       return True 
+    return False 
 
+def check_user(user,password,conn) :
+    if user in users and users[user] == password : 
+        return True 
+    else : 
+        return False 
+
+sent = {}
+rec = {}
 
 # Create a TCP/IP socket
 class Server(Socket) : 
       def __init__(self, _socket , addr = None ):
           self.addr = addr  
           super().__init__(_socket)
-          self.url_maps = { "/test" : self.test }
+          self.preauth_url_maps = {"/signup" : self.signup , "/login" : self.login }
+          self.url_maps = { "/test" : self.test , "/send_msg"  : self.send_msg }
       
-    #   def login(self,data,headers) :  
-    #        user,password = data["user"] , data["password"] 
-    #        if [user,password] in users : 
+      def login(self,data,headers) : 
+          isLogged = check_user(data["user"],data["password"],self.addr)
+          if isLogged :
+            self.is_authorised = True 
+          self.add_send_queue("/login_res", isLogged)
+          return isLogged
+      def signup(self,data,headers):
+          isCreated = create_user(data["user"],data["password"])
+          self.add_send_queue("/signup_res", isCreated)
             
+      def send_msg(self,data,headers) :
+          _id = data["id"]
+          time.sleep(0.01)
+          
+          self.add_send_queue("/read_reciept", {"time" : time.time() } ,{ "id": _id })    
+      
+
       def test(self,data,headers) : 
           if data.count("test messsage number") != 1000 : 
               raise Exception("full not recieved" + data[:10])
           print('received {!r}'.format(data.replace("test messsage number","")) + " :: " + str(self.addr[1]) + " :: " + str(headers) ) 
         
 
+
 sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
 server_address = ('localhost', 10000)
 print('Starting up on {} port {}'.format(*server_address))
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
 sock.bind(server_address)
 
 sock.listen(100)
