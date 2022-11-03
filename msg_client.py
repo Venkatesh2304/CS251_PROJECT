3ff772f7f98a6e3d4803c2e7e087c5efebfc5cde
Venkatesh2304 <96430022+Venkatesh2304@users.noreply.github.com>
2022-11-04 02:29:03 +0530

    db schema

---
+++ msg_client.py
@@ -0,0 +1,6 @@
+# function to add sent msg with other attributes 
+# function to add read_reciept or read_Reciepts id and recieved time from server ( id also comes )
+# same as above from client 
+# function to get offline messages 
+# function to add recieved msg or msgs with recieved time 
+
--- msg_serverdb.py
+++ msg_serverdb.py
@@ -2,63 +2,82 @@ from connectdb import connectToDB
 conn = connectToDB()
 
 #adds a message sent to database
-def addMessage(id,sender,reciever,message,typ,timesent,timeseen):
+#checks for _id collision 
+#return the new id even if already inserted 
+def addMessage(_id,sender,reciever,message,typ,sent):
+    #remove timeseen , add old _id (_id) while adding check wheter the old _id for that sender is already there 
     cur = conn.cursor()
     cur.execute(f"""INSERT INTO msg_server(ID,SENDER,RECIEVER,MESSAGE,TYPE,STATUS,TIME_SENT,TIME_SEEN) VALUES ({id},'{sender}','{reciever}','{message}','{typ}','NR',TIMESTAMP '{timesent}',TIMESTAMP '{timeseen}')""")
     conn.commit()
     cur.execute("SELECT * FROM msg_server")
-  
+      
 #gives all the unrecieved messages of a user
+#rtype [[]] 
 def getAllUnrecievedMsg(user):
     cur = conn.cursor()
     cur.execute(f"""SELECT ID,SENDER,MESSAGE,TYPE,TIME_SENT,TIME_SEEN FROM msg_server WHERE RECIEVER='{user}' AND STATUS='NR'""")
     msg_list = cur.fetchall()
     return msg_list
 
-#gives all unread but recieved messages from a contact of a user 
-def getUnreadtMsg(user,contact):
-    cur = conn.cursor()
-    cur.execute(f"""SELECT ID,MESSAGE,TYPE,STATUS,TIME_SENT,TIME_SEEN FROM msg_server WHERE RECIEVER='{user}' AND SENDER='{contact}' AND STATUS='R'""")
-    msg_list = cur.fetchall()
-    return msg_list
-
-#gives all unrecieved messages from a contact of a user 
-def getUnrecievedtMsg(user,contact):
-    cur = conn.cursor()
-    cur.execute(f"""SELECT ID,MESSAGE,TYPE,STATUS,TIME_SENT,TIME_SEEN FROM msg_server WHERE RECIEVER='{user}' AND SENDER='{contact}' AND STATUS='NR'""")
-    msg_list = cur.fetchall()
-    return msg_list 
-
-#gives all seen messages from a contact of a user 
-def getReadtMsg(user,contact):
-    cur = conn.cursor()
-    cur.execute(f"""SELECT ID,MESSAGE,TYPE,STATUS,TIME_SENT,TIME_SEEN FROM msg_server WHERE RECIEVER='{user}' AND SENDER='{contact}' AND STATUS='S'""")
-    msg_list = cur.fetchall()
-    return msg_list
-
 #removes the message with the given id, useless ig
-def removeMessage(id):
+#id ,reciever 
+#[id] , reciever
+def removeMessage(id,):
     cur = conn.cursor()
     cur.execute(f"""DELETE FROM msg_server WHERE ID={id}""")
     conn.commit()
 
-#removes messages seen from a contact of a user 
-def removeReadMessages(user,contact):
-    cur = conn.cursor()
-    cur.execute(f"""DELETE FROM msg_server WHERE RECIEVER='{user}' AND SENDER='{contact}' AND STATUS='S'""")
-    conn.commit()
+#function to update the id, recieved time  ,receiver (verify)
+#delete msg [id] 
+  
 
-#updates the status of messages seen by user
-def updateSeenStatus(user,contact,time_seen):
-    cur=conn.cursor()
-    cur.execute(f"""UPDATE msg_server SET STATUS='S', TIME_SEEN = TIMESTAMP '{time_seen}' WHERE RECIEVER='{user}' AND SENDER='{contact}'""")
-    conn.commit()
 
-#updates the status of messages recieved by user
-def updateRecievedStatus(user):
-    cur=conn.cursor()
-    cur.execute(f"""UPDATE msg_server SET STATUS='R' WHERE RECIEVER='{user}' AND STATUS='NR'""")
-    conn.commit()
+
+
+
+
+
+
+
+
+
+
+# #updates the status of messages recieved by user
+# def updateRecievedStatus(user):
+#     cur=conn.cursor()
+#     cur.execute(f"""UPDATE msg_server SET STATUS='R' WHERE RECIEVER='{user}' AND STATUS='NR'""")
+#     conn.commit()
 
 #STATUS = S/R/NR (seen,recieved,not recieved)
 #give time as datetime.datetime.now(timezone.utc)
+#gives all unread but recieved messages from a contact of a user 
+# def getUnreadtMsg(user,contact):
+#     cur = conn.cursor()
+#     cur.execute(f"""SELECT ID,MESSAGE,TYPE,STATUS,TIME_SENT,TIME_SEEN FROM msg_server WHERE RECIEVER='{user}' AND SENDER='{contact}' AND STATUS='R'""")
+#     msg_list = cur.fetchall()
+#     return msg_list
+
+# #gives all unrecieved messages from a contact of a user 
+# def getUnrecievedtMsg(user,contact):
+#     cur = conn.cursor()
+#     cur.execute(f"""SELECT ID,MESSAGE,TYPE,STATUS,TIME_SENT,TIME_SEEN FROM msg_server WHERE RECIEVER='{user}' AND SENDER='{contact}' AND STATUS='NR'""")
+#     msg_list = cur.fetchall()
+#     return msg_list 
+
+#gives all seen messages from a contact of a user 
+# def getReadtMsg(user,contact):
+#     cur = conn.cursor()
+#     cur.execute(f"""SELECT ID,MESSAGE,TYPE,STATUS,TIME_SENT,TIME_SEEN FROM msg_server WHERE RECIEVER='{user}' AND SENDER='{contact}' AND STATUS='S'""")
+#     msg_list = cur.fetchall()
+#     return msg_list
+
+# removes messages seen from a contact of a user 
+# def removeReadMessages(user,contact):
+#     cur = conn.cursor()
+#     cur.execute(f"""DELETE FROM msg_server WHERE RECIEVER='{user}' AND SENDER='{contact}' AND STATUS='S'""")
+#     conn.commit()
+#updates the status of messages seen by user
+# def updateSeenStatus(user,contact,time_seen):
+#     cur=conn.cursor()
+#     cur.execute(f"""UPDATE msg_server SET STATUS='S', TIME_SEEN = TIMESTAMP '{time_seen}' WHERE RECIEVER='{user}' AND SENDER='{contact}'""")
+#     conn.commit()
