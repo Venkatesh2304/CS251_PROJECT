5edb59252a90aa4be458eeb6bf1b53c26328c968
Venkatesh2304 <96430022+Venkatesh2304@users.noreply.github.com>
2022-11-22 20:11:01 +0530

    .

--- createdb.py
+++ createdb.py
@@ -19,15 +19,15 @@ cur.execute("""CREATE TABLE msg_server(
              PRIMARY KEY(SENDER,OID));""")
 cur.execute("CREATE INDEX RECIEVER_INDEX ON msg_server(RECIEVER)")
 
-# cur.execute("""DROP TABLE IF EXISTS msg_client_sent""")
-# cur.execute("""CREATE TABLE msg_client_sent(
-#              ID INTEGER PRIMARY KEY,
-#              RECIEVER TEXT,
-#              MESSAGE TEXT,
-#              TYPE TEXT,
-#              TIME_SENT TIMESTAMP,
-#              TIME_RECIEVED TIMESTAMP);""")
-# cur.execute("CREATE INDEX RECIEVER_INDEX ON msg_client_sent(RECIEVER)")
+cur.execute("""DROP TABLE IF EXISTS msg_client_sent""")
+cur.execute("""CREATE TABLE msg_client_sent(
+             ID INTEGER PRIMARY KEY,
+             RECIEVER TEXT,
+             MESSAGE TEXT,
+             TYPE TEXT,
+             TIME_SENT TIMESTAMP,
+             TIME_RECIEVED TIMESTAMP);""")
+#cur.execute("CREATE INDEX RECIEVER_INDEX ON msg_client_sent(RECIEVER)")
 
 cur.execute("""DROP TABLE IF EXISTS msg_client_recieved""")
 cur.execute("""CREATE TABLE msg_client_recieved(
